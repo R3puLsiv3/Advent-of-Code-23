46def03d4e4db3c64df190c40925ff13f8960743
Domi <62677969+R3puLsiv3@users.noreply.github.com>
2023-12-09 19:11:23 +0100

    Day 9 solutions

---
+++ 9/Puzzle 2/puzzle_2.py
@@ -0,0 +1,27 @@
+
+def main():
+    with open("input.txt") as f:
+        data = f.read()
+
+    lines = data.splitlines()
+
+    sequences = []
+    for line in lines:
+        sequence = [int(x) for x in line.split()]
+        sequence.reverse()
+        sequences.append(sequence)
+
+    result = 0
+    for sequence in sequences:
+        length = len(sequence) - 1
+        while not all(number == 0 for number in sequence[:length]):
+            for index in range(length):
+                sequence[index] = sequence[index + 1] - sequence[index]
+            length -= 1
+        result += sum(sequence)
+
+    print(result)
+
+
+if __name__ == '__main__':
+    main()
