50dc310269a5d18a984a490dc70d63d03b31d981
Domi <62677969+R3puLsiv3@users.noreply.github.com>
2024-12-01 13:49:54 +0100

    Day 1 solutions.

--- 2024/1/Puzzle 1/puzzle_1.py
+++ 2024/1/Puzzle 1/puzzle_1.py
@@ -1,7 +1,25 @@
+import operator
+
 
 def main():
     with open("input.txt") as f:
         data = f.read()
+
+    lines = data.split("\n")
+    l1 = []
+    l2 = []
+    for line in lines:
+        values = line.split("   ")
+        left = int(values[0])
+        l1.append(left)
+        right = int(values[1])
+        l2.append(right)
+
+    l1.sort()
+    l2.sort()
+
+    result = sum([abs(x - y) for (x, y) in zip(l1, l2)])
+    print(result)
 
 if __name__ == '__main__':
     main()
--- 2024/1/Puzzle 2/puzzle_2_.py
+++ 2024/1/Puzzle 2/puzzle_2_.py
@@ -3,5 +3,23 @@ def main():
     with open("input.txt") as f:
         data = f.read()
 
+    lines = data.split("\n")
+    l1 = []
+    occurs_in_l2 = {}
+    for line in lines:
+        values = line.split("   ")
+        left = int(values[0])
+        l1.append(left)
+        if left not in occurs_in_l2:
+            occurs_in_l2[left] = 0
+        right = int(values[1])
+        if right in occurs_in_l2:
+            occurs_in_l2[right] += 1
+        else:
+            occurs_in_l2[right] = 1
+
+    result = sum([x * occurs_in_l2[x] for x in l1])
+    print(result)
+
 if __name__ == '__main__':
     main()
